c60055cd68674f4818a874ec1fc56a8763ab52f4
Wesley Bernard <wesley.bernard64@gmail.com>
2022-03-25 10:12:58 -0400

    first commit with get started code from lwjgl included

---
+++ src/main/java/main/main.java
@@ -0,0 +1,20 @@
+package main;
+
+import org.lwjgl.*;
+import org.lwjgl.glfw.*;
+import org.lwjgl.opengl.*;
+import org.lwjgl.system.*;
+
+import java.nio.*;
+
+import static org.lwjgl.glfw.Callbacks.*;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryStack.*;
+import static org.lwjgl.system.MemoryUtil.*;
+
+public class main {
+    public static void main(String[] args) {
+
+    }
+}
